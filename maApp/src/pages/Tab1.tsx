5c839db6db0ef134426fb9b8a9107f95b7e61dd9
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-03 08:30:36 +0200

    20221003

--- maApp/src/pages/Tab1.tsx
+++ maApp/src/pages/Tab1.tsx
@@ -1,22 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonModal, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardContent, IonItem, IonLabel, IonInput, IonButton  } from '@ionic/react';
 import './Tab1.css';
+import {useForm} from "react-hook-form"
+import React, { useState } from 'react';
 
 const Tab1: React.FC = () => {
+
+
+  const initialValues = {
+    input_id1:"",
+    input_id2: "", 
+    input_id3:""
+  }
+
+  const {register, handleSubmit}= useForm({
+    defaultValues : initialValues
+  })
+
+  const onSubmit = (data:any) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+  
+
+  const [showModal, setShowModal] = useState(false);
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab</IonTitle>
+          <IonTitle>Input practises</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab page" />
+        <IonCard>
+          <IonCardHeader>
+            Test Input Form
+          </IonCardHeader>
+
+
+          <IonContent>
+            <IonModal isOpen={showModal}>
+              <IonButton onClick={() => setShowModal(false)}>
+                  Close Modal
+              </IonButton>
+            </IonModal>
+          </IonContent>
+
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+          <IonCardContent>
+            <IonItem>
+                <IonLabel>Title1</IonLabel>
+                <IonInput {...register("input_id1", { required: true })}  />
+            </IonItem>
+            <IonItem>
+                <IonLabel>Title2</IonLabel>
+                <IonInput {...register("input_id2", { required: true })} />
+            </IonItem>
+            <IonItem>
+                <IonLabel>Title3</IonLabel>
+                <IonInput {...register("input_id3", { required: true })}  />
+            </IonItem>
+          <IonButton type="submit">SAVE</IonButton>
+          </IonCardContent>
+          </form>
+
+
+        </IonCard>
       </IonContent>
     </IonPage>
   );
